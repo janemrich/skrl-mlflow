fddfd4874b31776fa26a366266c46f365c418dae
Antonio Serrano Muñoz <toni.semu@gmail.com>
2025-07-29 15:50:21 -0400

    Clamp using same dtype as source

--- skrl/utils/framework/warp/ops.py
+++ skrl/utils/framework/warp/ops.py
@@ -9,25 +9,25 @@ __all__ = ["clamp", "concatenate", "convert_to_numpy_in_place"]
 @wp.kernel
 def _clamp_1d(src: wp.array(ndim=1), min: wp.array(ndim=1), max: wp.array(ndim=1), dst: wp.array(ndim=1)):
     i = wp.tid()
-    dst[i] = wp.clamp(src[i], min[i], max[i])
+    dst[i] = wp.clamp(src[i], src.dtype(min[i]), src.dtype(max[i]))
 
 
 @wp.kernel
 def _clamp_2d(src: wp.array(ndim=2), min: wp.array(ndim=1), max: wp.array(ndim=1), dst: wp.array(ndim=2)):
     i, j = wp.tid()
-    dst[i, j] = wp.clamp(src[i, j], min[j], max[j])
+    dst[i, j] = wp.clamp(src[i, j], src.dtype(min[j]), src.dtype(max[j]))
 
 
 @wp.kernel
 def _clamp_3d(src: wp.array(ndim=3), min: wp.array(ndim=1), max: wp.array(ndim=1), dst: wp.array(ndim=3)):
     i, j, k = wp.tid()
-    dst[i, j, k] = wp.clamp(src[i, j, k], min[k], max[k])
+    dst[i, j, k] = wp.clamp(src[i, j, k], src.dtype(min[k]), src.dtype(max[k]))
 
 
 @wp.kernel
 def _clamp_4d(src: wp.array(ndim=4), min: wp.array(ndim=1), max: wp.array(ndim=1), dst: wp.array(ndim=4)):
     i, j, k, l = wp.tid()
-    dst[i, j, k, l] = wp.clamp(src[i, j, k, l], min[l], max[l])
+    dst[i, j, k, l] = wp.clamp(src[i, j, k, l], src.dtype(min[l]), src.dtype(max[l]))
 
 
 _clamp = [None, _clamp_1d, _clamp_2d, _clamp_3d, _clamp_4d]
