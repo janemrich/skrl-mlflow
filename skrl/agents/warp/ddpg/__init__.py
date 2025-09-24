463a950b2e9f30230147026ea54b9a33dd69be00
Antonio Serrano Muñoz <toni.semu@gmail.com>
2025-09-24 13:43:56 -0400

    Update DDPG

--- skrl/agents/warp/ddpg/__init__.py
+++ skrl/agents/warp/ddpg/__init__.py
@@ -1,1 +1,2 @@
-from skrl.agents.warp.ddpg.ddpg import DDPG, DDPG_DEFAULT_CONFIG
+from skrl.agents.warp.ddpg.ddpg import DDPG
+from skrl.agents.warp.ddpg.ddpg_cfg import DDPG_CFG
--- skrl/agents/warp/ddpg/ddpg.py
+++ skrl/agents/warp/ddpg/ddpg.py
@@ -12,55 +12,7 @@ from skrl.models.warp import Model
 from skrl.resources.optimizers.warp import Adam
 from skrl.utils import ScopedTimer
 
-
-# fmt: off
-# [start-config-dict-warp]
-DDPG_DEFAULT_CONFIG = {
-    "gradient_steps": 1,            # gradient steps
-    "batch_size": 64,               # training batch size
-
-    "discount_factor": 0.99,        # discount factor (gamma)
-    "polyak": 0.005,                # soft update hyperparameter (tau)
-
-    "actor_learning_rate": 1e-3,    # actor learning rate
-    "critic_learning_rate": 1e-3,   # critic learning rate
-    "learning_rate_scheduler": None,        # learning rate scheduler class (see torch.optim.lr_scheduler)
-    "learning_rate_scheduler_kwargs": {},   # learning rate scheduler's kwargs (e.g. {"step_size": 1e-3})
-
-    "observation_preprocessor": None,       # observation preprocessor class (see skrl.resources.preprocessors)
-    "observation_preprocessor_kwargs": {},  # observation preprocessor's kwargs (e.g. {"size": env.observation_space})
-    "state_preprocessor": None,             # state preprocessor class (see skrl.resources.preprocessors)
-    "state_preprocessor_kwargs": {},        # state preprocessor's kwargs (e.g. {"size": env.state_space})
-
-    "random_timesteps": 0,          # random exploration steps
-    "learning_starts": 0,           # learning starts after this many steps
-
-    "grad_norm_clip": 0,            # clipping coefficient for the norm of the gradients
-
-    "exploration": {
-        "noise": None,              # exploration noise (see skrl.resources.noises)
-        "noise_kwargs": {},         # exploration noise's kwargs (e.g. {"std": 0.1})
-        "initial_scale": 1.0,       # initial scale for the noise
-        "final_scale": 1e-3,        # final scale for the noise
-        "timesteps": None,          # timesteps for the noise decay
-    },
-
-    "rewards_shaper": None,         # rewards shaping function: Callable(reward, timestep, timesteps) -> reward
-
-    "experiment": {
-        "directory": "",            # experiment's parent directory
-        "experiment_name": "",      # experiment name
-        "write_interval": "auto",   # TensorBoard writing interval (timesteps)
-
-        "checkpoint_interval": "auto",      # interval for checkpoints (timesteps)
-        "store_separately": False,          # whether to store checkpoints separately
-
-        "wandb": False,             # whether to use Weights & Biases
-        "wandb_kwargs": {}          # wandb kwargs (see https://docs.wandb.ai/ref/python/init)
-    }
-}
-# [end-config-dict-warp]
-# fmt: on
+from .ddpg_cfg import DDPG_CFG
 
 
 def enable_grad(obj, *, enabled: bool):
@@ -147,9 +99,7 @@ class DDPG(Agent):
 
         :raises KeyError: If a configuration key is missing.
         """
-        # _cfg = copy.deepcopy(DDPG_DEFAULT_CONFIG)  # TODO: ValueError: ctypes objects containing pointers cannot be pickled
-        _cfg = DDPG_DEFAULT_CONFIG
-        _cfg.update(cfg if cfg is not None else {})
+        self.cfg: DDPG_CFG
         super().__init__(
             models=models,
             memory=memory,
@@ -157,7 +107,7 @@ class DDPG(Agent):
             state_space=state_space,
             action_space=action_space,
             device=device,
-            cfg=_cfg,
+            cfg=DDPG_CFG(**cfg) if isinstance(cfg, dict) else cfg,
         )
 
         # models
@@ -172,60 +122,38 @@ class DDPG(Agent):
         self.checkpoint_modules["critic"] = self.critic
         self.checkpoint_modules["target_critic"] = self.target_critic
 
-        # configuration
-        self._gradient_steps = self.cfg["gradient_steps"]
-        self._batch_size = self.cfg["batch_size"]
-
-        self._discount_factor = self.cfg["discount_factor"]
-        self._polyak = self.cfg["polyak"]
-
-        self._actor_learning_rate = self.cfg["actor_learning_rate"]
-        self._critic_learning_rate = self.cfg["critic_learning_rate"]
-        self._learning_rate_scheduler = self.cfg["learning_rate_scheduler"]
-
-        self._observation_preprocessor = self.cfg["observation_preprocessor"]
-        self._state_preprocessor = self.cfg["state_preprocessor"]
-
-        self._random_timesteps = self.cfg["random_timesteps"]
-        self._learning_starts = self.cfg["learning_starts"]
-
-        self._grad_norm_clip = self.cfg["grad_norm_clip"]
-
-        self._exploration_noise = self.cfg["exploration"]["noise"]
-        self._exploration_initial_scale = self.cfg["exploration"]["initial_scale"]
-        self._exploration_final_scale = self.cfg["exploration"]["final_scale"]
-        self._exploration_timesteps = self.cfg["exploration"]["timesteps"]
-
-        self._rewards_shaper = self.cfg["rewards_shaper"]
-
         # set up noise
-        if self._exploration_noise is not None:
-            self._exploration_noise = self._exploration_noise(**self.cfg["exploration"]["noise_kwargs"])
+        if self.cfg.exploration_noise is not None:
+            self._exploration_noise = self.cfg.exploration_noise(**self.cfg.exploration_noise_kwargs)
         else:
             logger.warning("agents:DDPG: No exploration noise specified, training performance may be degraded")
+            self._exploration_noise = None
 
         # set up optimizers and learning rate schedulers
         if self.policy is not None and self.critic is not None:
-            self.policy_optimizer = Adam(self.policy.parameters(), lr=self._actor_learning_rate)
-            self.critic_optimizer = Adam(self.critic.parameters(), lr=self._critic_learning_rate)
-            if self._learning_rate_scheduler is not None:
-                self.policy_scheduler = self._learning_rate_scheduler(
-                    self.policy_optimizer, **self.cfg["learning_rate_scheduler_kwargs"]
-                )
-                self.critic_scheduler = self._learning_rate_scheduler(
-                    self.critic_optimizer, **self.cfg["learning_rate_scheduler_kwargs"]
-                )
-
+            # - optimizers
+            self.policy_optimizer = Adam(self.policy.parameters(), lr=self.cfg.learning_rate[0])
+            self.critic_optimizer = Adam(self.critic.parameters(), lr=self.cfg.learning_rate[1])
             # self.checkpoint_modules["policy_optimizer"] = self.policy_optimizer
             # self.checkpoint_modules["critic_optimizer"] = self.critic_optimizer
+            # - learning rate schedulers
+            self.policy_scheduler = self.cfg.learning_rate_scheduler[0]
+            self.critic_scheduler = self.cfg.learning_rate_scheduler[1]
+            if self.policy_scheduler is not None:
+                self.policy_scheduler = self.cfg.learning_rate_scheduler[0](
+                    self.policy_optimizer, **self.cfg.learning_rate_scheduler_kwargs[0]
+                )
+            if self.critic_scheduler is not None:
+                self.critic_scheduler = self.cfg.learning_rate_scheduler[1](
+                    self.critic_optimizer, **self.cfg.learning_rate_scheduler_kwargs[1]
+                )
 
         # set up target networks
         if self.target_policy is not None and self.target_critic is not None:
-            # freeze target networks with respect to optimizers (update via .update_parameters())
+            # - freeze target networks with respect to optimizers (update via .update_parameters())
             self.target_policy.freeze_parameters(True)
             self.target_critic.freeze_parameters(True)
-
-            # update target networks (hard update)
+            # - update target networks (hard update)
             self.target_policy.update_parameters(self.policy, polyak=1)
             self.target_critic.update_parameters(self.critic, polyak=1)
 
@@ -235,16 +163,16 @@ class DDPG(Agent):
 
         # set up preprocessors
         # - observations
-        if self._observation_preprocessor:
-            self._observation_preprocessor = self._observation_preprocessor(
-                **self.cfg["observation_preprocessor_kwargs"]
+        if self.cfg.observation_preprocessor:
+            self._observation_preprocessor = self.cfg.observation_preprocessor(
+                **self.cfg.observation_preprocessor_kwargs
             )
             self.checkpoint_modules["observation_preprocessor"] = self._observation_preprocessor
         else:
             self._observation_preprocessor = self._empty_preprocessor
         # - states
-        if self._state_preprocessor:
-            self._state_preprocessor = self._state_preprocessor(**self.cfg["state_preprocessor_kwargs"])
+        if self.cfg.state_preprocessor:
+            self._state_preprocessor = self.cfg.state_preprocessor(**self.cfg.state_preprocessor_kwargs)
             self.checkpoint_modules["state_preprocessor"] = self._state_preprocessor
         else:
             self._state_preprocessor = self._empty_preprocessor
@@ -255,6 +183,7 @@ class DDPG(Agent):
         :param trainer_cfg: Trainer configuration.
         """
         super().init(trainer_cfg=trainer_cfg)
+        self.enable_models_training_mode(False)
 
         # create tensors in memory
         if self.memory is not None:
@@ -301,46 +230,28 @@ class DDPG(Agent):
             "states": self._state_preprocessor(states),
         }
         # sample random actions
-        if timestep < self._random_timesteps:
+        if timestep < self.cfg.random_timesteps:
             return self.policy.random_act(inputs, role="policy")
 
         # sample deterministic actions
         actions, outputs = self.policy.act(inputs, role="policy")
 
         # add exploration noise
-        if self._exploration_noise is not None:
-            # sample noises
+        if self._exploration_noise:
             noises = self._exploration_noise.sample(actions.shape)
+            scale = self.cfg.exploration_scheduler(timestep, timesteps) if self.cfg.exploration_scheduler else 1.0
+            # modify actions
+            wp.launch(
+                _apply_exploration_noise,
+                dim=actions.shape,
+                inputs=[actions, noises, self.clip_actions_min, self.clip_actions_max, float(scale)],
+                device=self.device,
+            )
 
-            # define exploration timesteps
-            scale = self._exploration_final_scale
-            if self._exploration_timesteps is None:
-                self._exploration_timesteps = timesteps
-
-            # apply exploration noise
-            if timestep <= self._exploration_timesteps:
-                scale = (1 - timestep / self._exploration_timesteps) * (
-                    self._exploration_initial_scale - self._exploration_final_scale
-                ) + self._exploration_final_scale
-
-                # modify actions
-                wp.launch(
-                    _apply_exploration_noise,
-                    dim=actions.shape,
-                    inputs=[actions, noises, self.clip_actions_min, self.clip_actions_max, scale],
-                    device=self.device,
-                )
-
-                # record noises
-                self.track_data("Exploration / Exploration noise (max)", np.max(noises.numpy()).item())
-                self.track_data("Exploration / Exploration noise (min)", np.min(noises.numpy()).item())
-                self.track_data("Exploration / Exploration noise (mean)", np.mean(noises.numpy()).item())
-
-            else:
-                # record noises
-                self.track_data("Exploration / Exploration noise (max)", 0)
-                self.track_data("Exploration / Exploration noise (min)", 0)
-                self.track_data("Exploration / Exploration noise (mean)", 0)
+            # record noises
+            self.track_data("Exploration / Exploration noise (max)", np.max(noises.numpy()).item())
+            self.track_data("Exploration / Exploration noise (min)", np.min(noises.numpy()).item())
+            self.track_data("Exploration / Exploration noise (mean)", np.mean(noises.numpy()).item())
 
         return actions, outputs
 
@@ -389,8 +300,8 @@ class DDPG(Agent):
 
         if self.memory is not None:
             # reward shaping
-            if self._rewards_shaper is not None:
-                rewards = self._rewards_shaper(rewards, timestep, timesteps)
+            if self.cfg.rewards_shaper is not None:
+                rewards = self.cfg.rewards_shaper(rewards, timestep, timesteps)
 
             # storage transition in memory
             self.memory.add_samples(
@@ -418,11 +329,11 @@ class DDPG(Agent):
         :param timestep: Current timestep.
         :param timesteps: Number of timesteps.
         """
-        if timestep >= self._learning_starts:
+        if timestep >= self.cfg.learning_starts:
             with ScopedTimer() as timer:
-                self.enable_training_mode(True)
+                self.enable_models_training_mode(True)
                 self.update(timestep=timestep, timesteps=timesteps)
-                self.enable_training_mode(False)
+                self.enable_models_training_mode(False)
                 self.track_data("Stats / Algorithm update time (ms)", timer.elapsed_time_ms)
 
         # write tracking data and checkpoints
@@ -436,7 +347,7 @@ class DDPG(Agent):
         """
 
         # gradient steps
-        for gradient_step in range(self._gradient_steps):
+        for gradient_step in range(self.cfg.gradient_steps):
 
             # sample a batch from memory
             (
@@ -448,7 +359,7 @@ class DDPG(Agent):
                 sampled_next_states,
                 sampled_terminated,
                 sampled_truncated,
-            ) = self.memory.sample(names=self._tensors_names, batch_size=self._batch_size)[0]
+            ) = self.memory.sample(names=self._tensors_names, batch_size=self.cfg.batch_size)[0]
 
             inputs = {
                 "observations": self._observation_preprocessor(sampled_observations, train=True),
@@ -480,7 +391,7 @@ class DDPG(Agent):
                         sampled_rewards,
                         sampled_terminated,
                         sampled_truncated,
-                        self._discount_factor,
+                        self.cfg.discount_factor,
                         np.prod(critic_values.shape),
                         self._critic_loss,
                     ],
@@ -515,12 +426,13 @@ class DDPG(Agent):
             tape.zero()
 
             # update target networks
-            self.target_policy.update_parameters(self.policy, polyak=self._polyak)
-            self.target_critic.update_parameters(self.critic, polyak=self._polyak)
+            self.target_policy.update_parameters(self.policy, polyak=self.cfg.polyak)
+            self.target_critic.update_parameters(self.critic, polyak=self.cfg.polyak)
 
             # update learning rate
-            if self._learning_rate_scheduler:
+            if self.policy_scheduler:
                 self.policy_scheduler.step()
+            if self.critic_scheduler:
                 self.critic_scheduler.step()
 
             # record data
@@ -535,6 +447,7 @@ class DDPG(Agent):
             # # self.track_data("Target / Target (min)", target_values.min().item())
             # # self.track_data("Target / Target (mean)", target_values.mean().item())
 
-            if self._learning_rate_scheduler:
+            if self.policy_scheduler:
                 self.track_data("Learning / Policy learning rate", self.policy_scheduler.get_last_lr()[0])
+            if self.critic_scheduler:
                 self.track_data("Learning / Critic learning rate", self.critic_scheduler.get_last_lr()[0])
