af52bc32a011794a27a2c8906b6722210f70038a
Toni-SM <toni.semu@gmail.com>
2025-09-24 11:49:05 -0400

    Implement agent/multi-agent and trainer configurations using Python Data Classes (#372)

--- skrl/agents/jax/dqn/__init__.py
+++ skrl/agents/jax/dqn/__init__.py
@@ -1,2 +1,4 @@
-from skrl.agents.jax.dqn.ddqn import DDQN, DDQN_DEFAULT_CONFIG
-from skrl.agents.jax.dqn.dqn import DQN, DQN_DEFAULT_CONFIG
+from skrl.agents.jax.dqn.ddqn import DDQN
+from skrl.agents.jax.dqn.ddqn_cfg import DDQN_CFG
+from skrl.agents.jax.dqn.dqn import DQN
+from skrl.agents.jax.dqn.dqn_cfg import DQN_CFG
